ced0211c72777b5d601aa2969f9531c810e36c2b
Bret Comnes <bcomnes@gmail.com>
2014-01-03 16:08:39 -0800

    Example file read and parse

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
-
+var fs = require('fs');
+var fm = require('front-matter');
+var path = require('path');
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  fs.readFile('_posts/2013-03-13-latex-section-font-sizing.md', 'utf8', function (err, data) {
+    if (err) throw (err);
+    var content = fm(data);
+    console.log(content);
+    res.render('index', { title: 'Express' });
+  });
 };
 
 exports.csstest = function(req, res){
